03b7423bb4457e9c264a380a761e507c1a0d6c24
Gustave Habimana <34315341+gustaveH@users.noreply.github.com>
2024-10-24 19:24:06 -0400

    Python basics

---
+++ hello.py
@@ -0,0 +1,67 @@
+# helloWorld = "Hello World!"
+# print(helloWorld )
+
+### QUOTATION ###
+# singleQuote = 'doesn\'t'
+# print(singleQuote)
+
+# # Single quote
+# doubleQuote = "\"Yes,\" they said."
+# print(doubleQuote)
+# # concatenated (glued together)
+# conc = "Gustave " "Habimana" " Muhoza"
+# print (conc)
+
+## concatenated variables(glued together)
+# firstWord = "Py"
+# secondWord = "thon"
+# print (firstWord + secondWord)
+
+### LISTS ###
+# earlyTwoThousand = [2000,2001,2002,2003]
+# lateTwoThousand = [2004,2005,2006,2008]
+
+# Replace
+# lateTwoThousand [3]= 2007
+
+# # .append is to add new items at the end of the list
+
+# lateTwoThousand.append(2009)
+
+# print (lateTwoThousand)
+
+# #indexing 
+# print(earlyTwoThousand [2])
+
+# print (earlyTwoThousand + lateTwoThousand)
+
+# names= ["John", "Craig", "Tom"]
+
+# maleNames = names
+# maleNames.append("Sean")
+# print(names)
+# print(names[0:2])
+
+#### BASIC FUNCTIONS ####
+# def greet():
+#     print("Hello, World!")
+# greet ()
+
+# with parameters
+# def greeting(name):            # Call the function with a parameter 
+#     print(f"Hello {name}")     # Print the function ...DON'T FORGET THE f
+# greeting ("Gustave")           # Define the parameter...as in literaly give the parameter a task 
+
+# def fullName (firstName, lastName):
+#     print(f"My name is {firstName} {lastName}")
+# fullName("Gustave", "Habimana")
+
+# def age (currentYear, yearBorn):
+#     print ({currentYear - yearBorn})
+# age(2024, 1991)
+
+# def sayHi(name="Stranger"):
+#     print(f"Hello {name}!")
+# sayHi("Fucker lol!")
+
+
